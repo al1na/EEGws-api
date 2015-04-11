1e1061ef705badf26231ecc88b4328993f8d264d
al1na <al1na@users.noreply.github.com>
2015-04-11 19:24:10 +0200

    implemented a couple more API calls, plus some other minor changes

--- eegws-api.py
+++ eegws-api.py
@@ -13,7 +13,7 @@ users_collection = db.users
 app = Flask(__name__)
 
 
-rec1 = {
+rec = {
         'timestamp': [1428262635, 1428262636, 1428262637, 1428262638, 1428262639],
         'device': 'emotiv',
         'electrodes': {'AF3': [10, 20, 33, 12, 56],
@@ -24,29 +24,12 @@ rec1 = {
                     'P7': [100, 90, 93, 90, 90]},
         'gyroscope': {'gyroX': [-100, 0, 150, 190, 200], 'gyroY': [-150, -90, 0, 80, 120]},
         'auxdata': {'battery': 100},
-        'annotation': u'Blinking experiment 05-04-2015',
+        'annotation': 'Blinking experiment 05-04-2015',
         'userid': 'd8f9419e-dbc2-11e4-b9d6-1681e6b88ec1',
         'good': True
         }
 
-rec2 = {
-        'timestamp': [1428265342, 1428265343, 1428265344, 1428265345, 1428265346],
-        'device': 'emotiv',
-        'frequency': 128,
-        'electrodes': {'AF3': [90, 90, 96, 100, 100],
-                       'O1': [100, 90, 90, 90, 90],
-                       'P7': [100, 90, 90, 90, 90]},
-        'quality': {'AF3': [10, 20, 33, 100, 100],
-                    'O1': [12, 57, 88, 100, 100],
-                    'P7': [22, 47, 78, 100, 100]},
-        'gyroscope': {'gyroX': [-100, 0, 150, 100, 100], 'gyroY': [-150, -90, 0, 100, 100]},
-        'auxdata': {'battery': 100},
-        'annotation': u'2nd blinking experiment 05-04-2015',
-        'good': True,
-        'userid': 'd8f9419e-dbc2-11e4-b9d6-1681e6b88ec1'
-        }
-
-user1 = {
+user = {
         'userid': 'd8f9419e-dbc2-11e4-b9d6-1681e6b88ec1',
         'username': 'al1na',
         'password': 'python',
@@ -56,9 +39,8 @@ user1 = {
         'public': True
         }
 
-#recordings_collection.insert(rec1)
-#recordings_collection.insert(rec2)
-#users_collection.insert(user1)
+#recordings_collection.insert(rec)
+#users_collection.insert(user)
 
 
 @auth.get_password
@@ -70,9 +52,11 @@ def get_password(username):
         password = u.get('password')
     return password
 
+
 @app.route('/mobileeg/api/v1/recordings', methods=['GET'])
 @auth.login_required
 def get_recordings():
+    #TODO: to provide only the public recordings (check if public=True for the user)
     recordings_list = []
     for recording in recordings_collection.find():
         recording.pop('_id')
@@ -81,20 +65,9 @@ def get_recordings():
     #return Response(json_util.dumps({'page' : recordings_list}), mimetype='application/json')
 
 
-"""
-@app.route('/mobileeg/api/v1/recordings/<int:recording_id>', methods=['GET'])
-@auth.login_required
-def get_recording(recording_id):
-    recording = [recording for recording in recordings_collection if recordings_collection['id'] == recording_id]
-    if len(recording) == 0:
-        abort(404)
-    return jsonify({'recording': recording[0]})
-"""
-
-
 @app.route('/mobileeg/api/v1/recordings', methods=['POST'])
 @auth.login_required
-def create_task():
+def create_recording():
     global userid
     if not request.json:
         abort(400)
@@ -115,9 +88,35 @@ def create_task():
             }
     recordings_collection.insert(rec)
     rec.pop('_id')
-    print rec
-    return jsonify({'recording':rec}), 201
+    return jsonify({'recording': rec}), 201
 
+
+@app.route('/mobileeg/api/v1/users', methods=['GET'])
+@auth.login_required
+def get_users():
+    users_list = []
+    for user in users_collection.find():
+        user.pop('_id')
+        users_list.append(user)
+    return jsonify({'recordings': users_list})
+
+
+@app.route('/mobileeg/api/v1/users', methods=['POST'])
+def create_user():
+    if not request.json:
+        abort(400)
+    user = {
+            'username': request.json['username'],
+            'password': request.json['password'],
+            'birthyear': request.json['birthyear'],
+            'gender': request.json['gender'],
+            'organization': request.json['organization'],
+            'public': request.json['public'],
+            'userid': request.json['userid']
+            }
+    users_collection.insert(user)
+    user.pop('_id')
+    return jsonify({'user': user}), 201
 
 """
 @app.route('/mobileeg/api/v1/recordings/<int:recording_id>', methods=['DELETE'])
@@ -127,6 +126,15 @@ def delete_recording(recording_id):
         abort(404)
     recordings_collection.remove(recording[0])
     return jsonify({'result': True})
+
+
+@app.route('/mobileeg/api/v1/recordings/<int:recording_id>', methods=['GET'])
+@auth.login_required
+def get_recording(recording_id):
+    recording = [recording for recording in recordings_collection if recordings_collection['id'] == recording_id]
+    if len(recording) == 0:
+        abort(404)
+    return jsonify({'recording': recording[0]})
 """
 
 
