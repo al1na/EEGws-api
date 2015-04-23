529ca7b1eb245e9df5c3698b843cb897d812e0b2
al1na <al1na@users.noreply.github.com>
2015-04-23 23:00:25 +0200

    extended the list of electrodes as in brede core.py

--- eegws-api.py
+++ eegws-api.py
@@ -14,9 +14,12 @@ from pylab import *
 from matplotlib import pyplot as plt
 from matplotlib import mlab
 
-ALL_ELECTRODES = ['F7', 'F8', 'AF3', 'AF4', 'FC5', 'FC6', 'F3',
-                    'F4', 'T7', 'T8', 'O1', 'O2', 'P7', 'P8']
 
+ALL_ELECTRODES = ['AF3', 'AF4', 'AF7', 'AF8', 'AFz', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'CP1', 'CP2', 'CP3', 'CP4', 'CP5',
+              'CP6', 'CPz', 'Cz', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'FC1', 'FC2', 'FC3', 'FC4', 'FC5',
+              'FC6', 'FCz', 'FP1', 'FP2', 'Fpz', 'FT7', 'FT8', 'Fz', 'Iz', 'Nz', 'P1', 'P2', 'P3', 'P4', 'P5', 'P6',
+              'P7', 'P8', 'PO3', 'PO4', 'PO7', 'PO8', 'POz', 'Pz', 'O1', 'O2', 'Oz', 'T7', 'T8', 'T9', 'T10', 'TP7',
+              'TP8', 'TP9', 'TP10']
 ELECTRODES = ['P7', 'AF3', 'O1']
 
 auth = HTTPBasicAuth()
